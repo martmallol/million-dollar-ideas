36dbe39975c265610623269a48c0941c8d1ca394
Martin Mallol <martinmallolcc@gmail.com>
2022-12-09 00:21:07 -0300

    1st form POST request. Navbar and links/add templates.

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,14 @@ const router = express.Router();
 
 const pool = require('../database'); // Connect DB
 
+// Search for 'localhost:4000/links/add'
+router.get('/add', (req, res) => {
+    res.render('links/add');
+})
+
+// Complete a multi-million dollar idea form and save it.
+router.post('/add', (req, res) => {
+    res.send('received');
+})
+
 module.exports = router;
