083f93f05b24b88d410d3a4da8168bf2de10562e
Martin Mallol <martinmallolcc@gmail.com>
2022-12-17 21:38:50 -0300

    GET links/list ideas listing

--- src/routes/links.js
+++ src/routes/links.js
@@ -6,7 +6,7 @@ const pool = require('../database'); // Connect DB
 // Search for 'localhost:4000/links/add'
 router.get('/add', (req, res) => {
     res.render('links/add');
-})
+});
 
 // Complete a multi-million dollar idea form and save it.
 router.post('/add', async (req, res) => {
@@ -19,8 +19,17 @@ router.post('/add', async (req, res) => {
     };
     // This petition will take time, that's why I include await
     // Adding the petition into the 'links' table in my database
+    // Query doesn't function with my PCs SQL
     await pool.query('INSERT INTO links set ?', [newIdea]);
     res.send('received');
-})
+});
+
+// Search for 'localhost:4000/links/'
+router.get('/', (req, res) => {
+    // Query doesn't function with my PCs SQL
+    const links = pool.query('SELECT * FROM links');
+    console.log(links);
+    res.render('links/list', { links });
+});
 
 module.exports = router;
