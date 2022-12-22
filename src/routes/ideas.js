649b8c044536f5fac93437b5f508208d6129bb18
Martin <martinmallolcc@gmail.com>
2022-12-22 16:47:59 -0300

    EDIT ideas

--- src/routes/ideas.js
+++ src/routes/ideas.js
@@ -41,9 +41,32 @@ router.get('/', async (req, res) => { // Why is it important to do async?
 // Delete the idea with that id
 router.get('/delete/:id', async (req, res) => {
     console.log(req.params.id); // Check if it send me the id
-    const { id } = await req.params;
-    pool.query('DELETE FROM ideas WHERE ID = ?', [id]);
+    const { id } = req.params;
+    await pool.query('DELETE FROM ideas WHERE ID = ?', [id]);
     res.redirect('/ideas'); // After deleting the idea, we get redirected to the main page again
 });
+
+// Edit the idea with that id
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    const idea = await pool.query('SELECT * FROM ideas WHERE id = ?', [id]);
+    console.log(idea[0]);
+    res.render('ideas/edit', {idea: idea[0]}); // Similar to the 'add' page
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, money, description } = req.body;
+    const newIdea = { // The body of the object I'm sending
+        title,
+        money,
+        description
+    };
+    // Update table on DB
+    pool.query('UPDATE ideas set ? WHERE id = ?', [newIdea, id]);
+    // After editing, redirect to the main page
+    res.redirect('/ideas');
+})
 
 module.exports = router;
