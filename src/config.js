f254fa57bb523ac5c071b624d3c47bf98b8d80c6
Martin <martinmallolcc@gmail.com>
2023-02-07 16:41:16 -0300

    module exports syntax

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 // Database env variables
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Pass123'
-export const DB_NAME = process.env.DB_NAME || 'database_links'
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Pass123';
+const DB_NAME = process.env.DB_NAME || 'database_links';
+
+module.exports = {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME};
