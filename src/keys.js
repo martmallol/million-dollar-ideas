c0e9bf7dc7042f6a923582663e005504029ed71b
Martin <martinmallolcc@gmail.com>
2023-02-07 16:17:08 -0300

    Pre deploy to Railway. Set environment db variables

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,15 @@
+import {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
+} from './config.js'
+
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'Pass123',
-        database: 'database_links'
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME
     }
 }
